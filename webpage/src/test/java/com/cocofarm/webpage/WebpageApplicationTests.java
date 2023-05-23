a5a82624dd73c85429082127dc7bff09e074cd76
yuudong123 <1mouri81@gmail.com>
2023-05-23 17:01:31 +0900

    first commit

---
+++ webpage/src/test/java/com/cocofarm/webpage/WebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocofarm.webpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
