f896fbe21b0aaddac88169ae3957ee2e1a4da5d0
yuudong123 <1mouri81@gmail.com>
2023-05-30 17:40:28 +0900

    앱 기본틀 좀 수정

---
+++ andapp/app/src/main/java/com/cocofarm/andapp/member/BannedActivity.java
@@ -0,0 +1,14 @@
+package com.cocofarm.andapp.member;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BannedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banned);
+    }
+}
