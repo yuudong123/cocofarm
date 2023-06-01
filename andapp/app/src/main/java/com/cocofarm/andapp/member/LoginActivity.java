d2b780e44f85704808094d6169d4203186a6abcf
jmg5909 <124036772+jmg5909@users.noreply.github.com>
2023-06-01 18:03:55 +0900

    layout id,color 일부 수정

--- andapp/app/src/main/java/com/cocofarm/andapp/member/LoginActivity.java
+++ andapp/app/src/main/java/com/cocofarm/andapp/member/LoginActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.cocofarm.andapp.R;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
