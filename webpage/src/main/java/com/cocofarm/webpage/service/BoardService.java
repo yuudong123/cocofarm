3cb495bc9ab4fa737814183f271cc89ead1033d8
yuudong123 <1mouri81@gmail.com>
2023-06-01 13:35:33 +0900

    여러 매퍼 사용을 위해 이름 변경

--- webpage/src/main/java/com/cocofarm/webpage/service/BoardService.java
+++ webpage/src/main/java/com/cocofarm/webpage/service/BoardService.java
@@ -6,34 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cocofarm.webpage.domain.BoardVO;
+import com.cocofarm.webpage.mapper.AttachMapper;
 import com.cocofarm.webpage.mapper.BoardMapper;
 
 @Service
 public class BoardService {
 
     @Autowired
-    BoardMapper mapper;
+    BoardMapper boardMapper;
+
+    @Autowired
+    AttachMapper attachMapper;
 
     public ArrayList<BoardVO> selectList(int code) {
-        ArrayList<BoardVO> list = mapper.selectList(code);
+        ArrayList<BoardVO> list = boardMapper.selectList(code);
         return list;
     }
 
     public BoardVO select(int board_no) {
-        BoardVO vo = mapper.select(board_no);
+        BoardVO vo = boardMapper.select(board_no);
         return vo;
     }
 
     public void insert(BoardVO vo) {
-        mapper.insert(vo);
+        boardMapper.insert(vo);
     }
 
     public void update(BoardVO vo) {
-        mapper.update(vo);
+        boardMapper.update(vo);
     }
 
     public void delete(int board_no) {
-        mapper.delete(board_no);
+        boardMapper.delete(board_no);
     }
 
 }
