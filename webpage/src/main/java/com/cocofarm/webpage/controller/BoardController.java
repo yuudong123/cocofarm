7e5652fe6a0719b66dc1f6794967328dbd54a144
yuudong123 <1mouri81@gmail.com>
2023-06-01 10:47:57 +0900

    매핑을 댓글과 구별할수있게 수정

--- webpage/src/main/java/com/cocofarm/webpage/controller/BoardController.java
+++ webpage/src/main/java/com/cocofarm/webpage/controller/BoardController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cocofarm.webpage.domain.BoardVO;
@@ -12,40 +14,68 @@ import com.cocofarm.webpage.service.BoardService;
 import com.google.gson.Gson;
 
 @Controller
+@RequestMapping(value = { "notice", "qna", "review" })
 public class BoardController {
 
     @Autowired
     BoardService service;
 
-    @PostMapping(value = "selectlist", produces = "text/html;charset=utf-8")
+    @GetMapping(value = "selectboardlist")
+    public ArrayList<BoardVO> selectBoardList(int code) {
+        ArrayList<BoardVO> list = service.selectList(code);
+        return list;
+    }
+
+    @GetMapping(value = "selectboard")
+    public BoardVO selectBoard(int board_no) {
+        BoardVO vo = service.select(board_no);
+        return vo;
+    }
+
+    @GetMapping(value = "insertboard")
+    public void insertBoard(BoardVO vo) {
+        service.insert(vo);
+    }
+
+    @GetMapping(value = "updateboard")
+    public void updateBoard(BoardVO vo) {
+        service.update(vo);
+    }
+
+    @GetMapping(value = "deleteboard")
+    public void deleteBoard(int board_no) {
+        service.delete(board_no);
+    }
+
+    @PostMapping(value = "selectboardlist.and", produces = "text/html;charset=utf-8")
     @ResponseBody
-    public String selectList(int code) {
+    public String selectBoardListAnd(int code) {
         ArrayList<BoardVO> list = service.selectList(code);
         return new Gson().toJson(list);
     }
 
-    @PostMapping(value = "select", produces = "text/html;charset=utf-8")
+    @PostMapping(value = "selectboard.and", produces = "text/html;charset=utf-8")
     @ResponseBody
-    public String select(int board_no) {
+    public String selectBoardAnd(int board_no) {
         BoardVO vo = service.select(board_no);
         return new Gson().toJson(vo);
     }
 
-    @PostMapping(value = "insert", produces = "text/html;charset=utf-8")
+    @PostMapping(value = "insertboard.and", produces = "text/html;charset=utf-8")
     @ResponseBody
-    public void insert(BoardVO vo) {
+    public void insertBoardAnd(BoardVO vo) {
         service.insert(vo);
     }
 
-    @PostMapping(value = "update", produces = "text/html;charset=utf-8")
+    @PostMapping(value = "updateboard.and", produces = "text/html;charset=utf-8")
     @ResponseBody
-    public void update(BoardVO vo) {
+    public void updateBoardAnd(BoardVO vo) {
         service.update(vo);
     }
 
-    @PostMapping(value = "delete", produces = "text/html;charset=utf-8")
+    @PostMapping(value = "deleteboard.and", produces = "text/html;charset=utf-8")
     @ResponseBody
-    public void delete(int board_no) {
+    public void deleteBoardAnd(int board_no) {
         service.delete(board_no);
     }
 
