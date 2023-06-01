b0567de71ca035797e76a0b3124b53d346fc4569
yuudong123 <1mouri81@gmail.com>
2023-06-01 17:03:14 +0900

    바텀 내비게이션 바 작업 (기능구현 아직)

--- andapp/app/src/main/java/com/cocofarm/andapp/MainActivity.java
+++ andapp/app/src/main/java/com/cocofarm/andapp/MainActivity.java
@@ -1,22 +1,25 @@
 package com.cocofarm.andapp;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import com.cocofarm.andapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
