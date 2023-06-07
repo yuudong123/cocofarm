8b57048165f83fe6a5e1fe28dd69998169f961d3
yuudong123 <1mouri81@gmail.com>
2023-06-07 15:13:44 +0900

    게시판 DB에서 불러오게 변경

--- andapp/app/src/main/java/com/cocofarm/andapp/board/NoticeFragment.java
+++ andapp/app/src/main/java/com/cocofarm/andapp/board/NoticeFragment.java
@@ -8,11 +8,13 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.cocofarm.andapp.common.CodeTable;
+import com.cocofarm.andapp.conn.CommonConn;
 import com.cocofarm.andapp.databinding.FragmentNoticeBinding;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class NoticeFragment extends Fragment {
 
@@ -22,20 +24,17 @@ public class NoticeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentNoticeBinding.inflate(inflater, container, false);
 
-        ArrayList<BoardVO> list = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            BoardVO vo = new BoardVO();
-            vo.setRownum(i+1);
-            vo.setTitle("제목테스트"+i);
-            vo.setNickname("사용자"+i);
-            vo.setRegdate(new Date());
-            vo.setBoard_category_cd(202);
-            list.add(vo);
-        }
-        NoticeAdapter adapter = new NoticeAdapter(list, getContext());
-        LinearLayoutManager manager = new LinearLayoutManager(getContext());
-        binding.recvBoardList.setAdapter(adapter);
-        binding.recvBoardList.setLayoutManager(manager);
+
+        CommonConn conn = new CommonConn(null, "notice/selectboardlist.and");
+        conn.addParam("code", CodeTable.BOARD_CATEGORY_NOTICE);
+        conn.onExcute((isResult, data) -> {
+            ArrayList<BoardVO> list = new Gson().fromJson(data, new TypeToken<ArrayList<BoardVO>>() {
+            }.getType());
+            NoticeAdapter adapter = new NoticeAdapter(list, getContext());
+            LinearLayoutManager manager = new LinearLayoutManager(getContext());
+            binding.recvBoardList.setAdapter(adapter);
+            binding.recvBoardList.setLayoutManager(manager);
+        });
 
         return binding.getRoot();
     }
