274ffb57700662d203b0b6c34ead38a71571c406
yuudong123 <1mouri81@gmail.com>
2023-06-07 10:07:41 +0900

    코드테이블 클래스 추가

---
+++ andapp/app/src/main/java/com/cocofarm/andapp/common/CodeTable.java
@@ -0,0 +1,11 @@
+package com.cocofarm.andapp.common;
+
+public class CodeTable {
+    public final int MEMBER_TYPE_ADMIN = 101;
+    public final int MEMBER_TYPE_NORMAL = 102;
+//    public final int MEMBER_TYPE_VIP = 103; 얘 딱히 필요없으면 사용 안하고, DB에서도 지우기
+    public final int BOARD_CATEGORY_QNA = 201;
+    public final int BOARD_CATEGORY_NOTICE = 202;
+    public final int BOARD_CATEGORY_REVIEW = 203;
+    public final int BOARD_CATEGORY_EVENT = 204;
+}
