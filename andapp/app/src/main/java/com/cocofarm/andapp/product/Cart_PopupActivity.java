543eec27e0e48824434e5058f93a10e5b7f848cb
ImJini <sejin376@naver.com>
2023-06-08 12:16:47 +0900

    장바구니 담았을때 팝업창

---
+++ andapp/app/src/main/java/com/cocofarm/andapp/product/Cart_PopupActivity.java
@@ -0,0 +1,16 @@
+package com.cocofarm.andapp.product;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cocofarm.andapp.R;
+
+public class Cart_PopupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_popup);
+    }
+}
