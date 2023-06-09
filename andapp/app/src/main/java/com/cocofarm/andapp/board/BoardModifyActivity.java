d278af6092c314464b33fca0084e8d908952b561
yuudong123 <1mouri81@gmail.com>
2023-06-09 09:57:26 +0900

    정렬

--- andapp/app/src/main/java/com/cocofarm/andapp/board/BoardModifyActivity.java
+++ andapp/app/src/main/java/com/cocofarm/andapp/board/BoardModifyActivity.java
@@ -1,26 +1,24 @@
 package com.cocofarm.andapp.board;
 
-import static com.cocofarm.andapp.common.CommonVal.loginMemberAdmin;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.cocofarm.andapp.conn.CommonConn;
 import com.cocofarm.andapp.databinding.ActivityBoardModifyBinding;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 public class BoardModifyActivity extends AppCompatActivity {
 
     ActivityBoardModifyBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityBoardModifyBinding.inflate(getLayoutInflater());
+        binding = ActivityBoardModifyBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         BoardVO vo = (BoardVO) getIntent().getSerializableExtra("BoardVO");
 
@@ -28,33 +26,33 @@ public class BoardModifyActivity extends AppCompatActivity {
         binding.edtContent.setText(vo.getContent());
 
         binding.btnConfirm.setOnClickListener(v -> {
-            if(binding.edtTitle.getText().toString().equals("")||binding.edtContent.getText().toString().equals("")){
+            if (binding.edtTitle.getText().toString().equals("") || binding.edtContent.getText().toString().equals("")) {
                 Toast.makeText(this, "제목과 내용을 확인해주세요", Toast.LENGTH_SHORT).show();
             }
             CommonConn conn = new CommonConn(this, "updateboard.and");
             conn.addParam("board_no", vo.getBoard_no());
-            conn.addParam("product_id",0);
-            conn.addParam("title",binding.edtTitle.getText().toString());
-            conn.addParam("content",binding.edtContent.getText().toString());
+            conn.addParam("product_id", 0);
+            conn.addParam("title", binding.edtTitle.getText().toString());
+            conn.addParam("content", binding.edtContent.getText().toString());
             conn.onExcute((isResult, data) -> {
-                Log.d("글 작성", "onCreate: "+isResult);
-                if(isResult) {
-                    if (BoardReadActivity.getInstance()!=null){
+                Log.d("글 작성", "onCreate: " + isResult);
+                if (isResult) {
+                    if (BoardReadActivity.getInstance() != null) {
                         BoardReadActivity.getInstance().finish();
                     }
                     this.finish();
-                    Intent intent = new Intent(BoardModifyActivity.this,BoardReadActivity.class);
-                    CommonConn newconn = new CommonConn(BoardModifyActivity.this,"selectboard.and");
+                    Intent intent = new Intent(BoardModifyActivity.this, BoardReadActivity.class);
+                    CommonConn newconn = new CommonConn(BoardModifyActivity.this, "selectboard.and");
                     newconn.addParam("board_no", vo.getBoard_no());
                     newconn.onExcute((isResult1, data1) -> {
-                        BoardVO newvo = (BoardVO) new Gson().fromJson(data1,BoardVO.class);
+                        BoardVO newvo = (BoardVO) new Gson().fromJson(data1, BoardVO.class);
                         intent.putExtra("BoardVO", newvo);
                         startActivity(intent);
                     });
                 }
             });
         });
-        binding.btnCancel.setOnClickListener(v->{
+        binding.btnCancel.setOnClickListener(v -> {
             finish();
         });
     }
