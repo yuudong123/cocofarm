bcabd8cc0668c0b3635ac2c1f3686f0453613893
yuudong123 <1mouri81@gmail.com>
2023-06-09 14:11:12 +0900

    이미지 처리 ImageUtil.load(context, imageView, filename) 사용가능하게 추가

---
+++ andapp/app/src/main/java/com/cocofarm/andapp/image/ImageDTO.java
@@ -0,0 +1,11 @@
+package com.cocofarm.andapp.image;
+
+import lombok.Data;
+
+@Data
+public class ImageDTO {
+    private String filename;
+    private int product_id;
+    private String isusing;
+}
+
---
+++ webpage/src/main/java/com/cocofarm/webpage/mapper/ImageMapper.java
@@ -0,0 +1,12 @@
+package com.cocofarm.webpage.mapper;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.cocofarm.webpage.domain.ImageDTO;
+
+@Mapper
+public interface ImageMapper {
+    public ArrayList<ImageDTO> selectImageList();
+}
