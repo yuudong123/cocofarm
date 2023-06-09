fea7ed51293313c7a974835c89c1e8775c101ccd
ImJini <sejin376@naver.com>
2023-06-09 14:52:40 +0900

    바텀시트 가격나오게 수정

--- andapp/app/src/main/java/com/cocofarm/andapp/product/DeviceAdapter.java
+++ andapp/app/src/main/java/com/cocofarm/andapp/product/DeviceAdapter.java
@@ -1,5 +1,7 @@
 package com.cocofarm.andapp.product;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,18 +12,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.cocofarm.andapp.databinding.ItemProductDeviceBinding;
 
 public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder> {
+    Context context;
 
     ItemProductDeviceBinding binding;
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-      binding=ItemProductDeviceBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        context = parent.getContext();
+      binding=ItemProductDeviceBinding.inflate(LayoutInflater.from(context),parent,false);
       return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int i) {
+        holder.binding.layoutProductDeviceItem.setOnClickListener(view -> {
+            Intent intent = new Intent(context , ProductActivity.class);
+            context.startActivity(intent);
+        });
 
     }
 
--- andapp/app/src/main/java/com/cocofarm/andapp/product/ProductActivity.java
+++ andapp/app/src/main/java/com/cocofarm/andapp/product/ProductActivity.java
@@ -2,15 +2,19 @@ package com.cocofarm.andapp.product;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cocofarm.andapp.R;
 import com.cocofarm.andapp.databinding.ActivityProductBinding;
 import com.cocofarm.andapp.databinding.BtnSheetProductBinding;
 import com.cocofarm.andapp.databinding.FragmentProductBinding;
+import com.cocofarm.andapp.order.CartActivity;
+import com.cocofarm.andapp.order.OrderActivity;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import lombok.val;
@@ -18,9 +22,8 @@ import lombok.val;
 public class ProductActivity extends AppCompatActivity {
 
    ActivityProductBinding binding;
-   Button btn_minus, btn_plus;
-   TextView tv_product_buy_amount;
    BtnSheetProductBinding bindingSheet;
+
     int number = 0;
 
 
@@ -31,27 +34,50 @@ public class ProductActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
+
+        bindingSheet = BtnSheetProductBinding.inflate(getLayoutInflater(), null , false);
         binding.btnProductBuy.setOnClickListener(v->{
-            bindingSheet = BtnSheetProductBinding.inflate(getLayoutInflater(), null , false);
+
 
             BottomSheetDialog dialog = new BottomSheetDialog(this);
 
             dialog.setContentView(bindingSheet.getRoot());
             dialog.show();
-
-
                 bindingSheet.btnMinus.setOnClickListener(view->{
                     if(number !=0){
                         number--;
                     }
                 bindingSheet.tvProductBuyAmount.setText(number+"");
-
+                allPrice();
             });
             bindingSheet.btnPlus.setOnClickListener(view->{
                 number++;
                 bindingSheet.tvProductBuyAmount.setText(number+"");
+                allPrice();
             });
         });
+        allPrice();
+
+        if(number != 0){
+        bindingSheet.btnGoCart.setOnClickListener(view-> {
+                    Intent intent = new Intent(ProductActivity.this, CartActivity.class);
+                    startActivity(intent);
+                });
+        }
+        if(number != 0) {
+            bindingSheet.btnGoBuy.setOnClickListener(view -> {
+                Intent intent = new Intent(ProductActivity.this, OrderActivity.class);
+                startActivity(intent);
+            });
+        }
+        else {
+          
+    }
+
+
+
+        //tv_all_price에 tv_product_buy_amount*tv_sheet_order_price 한 가격을 넣어줘야 함.
+
 
            // tv_product_buy_amount = findViewById(R.id.tv_product_buy_amount);
 
@@ -64,11 +90,21 @@ public class ProductActivity extends AppCompatActivity {
 //
 //            );
 
+
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         binding=null;
+    }
+
+    public void allPrice(){
+        int value1 = number;
+        int value2 = Integer.parseInt(bindingSheet.tvSheetOrderPrice.getText().toString());
+
+        int result = value1 * value2;
+        bindingSheet.tvAllPrice.setText(String.valueOf(result));
     }
 }
