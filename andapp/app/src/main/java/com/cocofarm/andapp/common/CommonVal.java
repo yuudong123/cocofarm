c024da603d59e3346c8305b0732103793afe634b
yuudong123 <1mouri81@gmail.com>
2023-06-11 13:19:25 +0900

    Update CommonVal.java

--- andapp/app/src/main/java/com/cocofarm/andapp/common/CommonVal.java
+++ andapp/app/src/main/java/com/cocofarm/andapp/common/CommonVal.java
@@ -15,24 +15,4 @@ public class CommonVal {
     public static SimpleDateFormat HHmmss = new SimpleDateFormat("HH:mm:ss");
 
     public static String boardselectedImage = null;
-//    private void getHashKey(){
-//        PackageInfo packageInfo = null;
-//        try {
-//            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-//        } catch (PackageManager.NameNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        if (packageInfo == null)
-//            Log.e("KeyHash", "KeyHash:null");
-//
-//        for (Signature signature : packageInfo.signatures) {
-//            try {
-//                MessageDigest md = MessageDigest.getInstance("SHA");
-//                md.update(signature.toByteArray());
-//                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-//            } catch (NoSuchAlgorithmException e) {
-//                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
-//            }
-//        }
-//    }
 }
