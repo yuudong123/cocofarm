06a95d6594fe054bdd2f006890c2ecc168fe73f3
ImJini <sejin376@naver.com>
2023-06-21 17:59:56 +0900

    결과처리용 데이터 잘 입력했는지 확인용 VO추가

---
+++ andapp/app/src/main/java/com/cocofarm/andapp/common/ResultVO.java
@@ -0,0 +1,9 @@
+package com.cocofarm.andapp.common;
+
+import lombok.Data;
+
+@Data
+public class ResultVO {
+    String result;
+    int resultCode;
+}
---
+++ webpage/src/main/java/com/cocofarm/webpage/common/ResultVO.java
@@ -0,0 +1,9 @@
+package com.cocofarm.webpage.common;
+
+import lombok.Data;
+
+@Data
+public class ResultVO {
+    String result;
+    int resultCode;
+}
