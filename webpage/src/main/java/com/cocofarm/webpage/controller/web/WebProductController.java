b7576e1c7e42e14b422f0af8cdb2475960fc51d4
yuudong123 <1mouri81@gmail.com>
2023-06-30 15:29:33 +0900

    웹 product 기본틀만 만듦

---
+++ webpage/src/main/java/com/cocofarm/webpage/controller/web/WebProductController.java
@@ -0,0 +1,10 @@
+package com.cocofarm.webpage.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@SessionAttributes("userinfo")
+@Controller
+public class WebProductController {
+
+}
