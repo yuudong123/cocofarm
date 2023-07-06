763c3e9392ef8652a2e000e6270da3798b4fbdb6
yuudong123 <1mouri81@gmail.com>
2023-07-06 13:35:48 +0900

    카테고리 js파일하나로 뽑음

---
+++ webpage/src/main/resources/static/js/category.js
@@ -0,0 +1,24 @@
+const categorys = document.querySelectorAll("#category a");
+const selectBar = document.getElementById("categorySelectBar");
+
+if (categorys != null) {
+  categorys.forEach((category) => {
+    category.addEventListener("click", () => {
+      reqDiv.setAttribute("action", "/board/" + category.dataset.category);
+      inputPage.setAttribute("value", "1");
+      reqDiv.submit();
+    });
+
+    var width = category.offsetWidth;
+    var offsetLeft = category.offsetLeft;
+    category.addEventListener("mouseenter", function () {
+      selectBar.style.width = width + "px";
+      selectBar.style.transform = "translateX(" + offsetLeft + "px)";
+    });
+
+    category.addEventListener("mouseleave", function () {
+      selectBar.style.width = "0";
+      selectBar.style.transform = "translateX(" + (offsetLeft + width / 2) + "px)";
+    });
+  });
+}
