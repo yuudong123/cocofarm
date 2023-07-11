7b22c73e9d6863a1144cee770acfa27edb4b4655
jmg5909 <124036772+jmg5909@users.noreply.github.com>
2023-07-11 19:12:56 +0900

    0711 Mydevice추가

---
+++ webpage/src/main/java/com/cocofarm/webpage/service/MyDeviceService.java
@@ -0,0 +1,23 @@
+package com.cocofarm.webpage.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cocofarm.webpage.domain.MemberVO;
+import com.cocofarm.webpage.domain.MyDeviceVO;
+import com.cocofarm.webpage.mapper.MyDeviceMapper;
+
+@Service
+public class MyDeviceService {
+    
+  @Autowired
+  MyDeviceMapper mapper;
+
+  public MyDeviceVO mydevice(int member_no) {
+    return mapper.mydevice(member_no);
+  }
+
+  public int add_device(MyDeviceVO vo) {
+    return mapper.add_device(vo);
+  }
+}
